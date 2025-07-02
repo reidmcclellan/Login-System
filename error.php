3d90292fbc27a95b6ed65d09151f47e3ce46487a
Reid McClellan <124468820+reidmcclellan@users.noreply.github.com>
2025-07-02 19:24:58 -0400

    Create error.php

---
+++ error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login Failed</title>
+</head>
+<body>
+  <h1>Login Failed</h1>
+  <p>Invalid username or password.</p>
+  <a href="login.php">Try Again</a>
+</body>
+</html>
